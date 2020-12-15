8124c4a8bacb931d02c74e6dc8d8d949b3860f05
vetrivel-muthusamy <vetrivel.muthusamy@hotmail.com>
2020-12-15 21:10:02 +0530

    Stage 1/7: Game log - Implementation completed

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -2,6 +2,15 @@ package bullscows;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        //System.out.println("Hello World!");
+        System.out.println("The secret code is prepared: ****.");
+        System.out.println("Turn 1. Answer:");
+        System.out.println("1234");
+        System.out.println("Grade: None.");
+        System.out.println("Turn 2. Answer:");
+        System.out.println("9876");
+        System.out.println("Grade: 4 bulls.");
+        System.out.println("Congrats! The secret code is 9876.");
     }
 }
