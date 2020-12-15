adc2ea0c7615dbcb7f95852c5e9123975401addc
vetrivel-muthusamy <vetrivel.muthusamy@hotmail.com>
2020-12-15 21:13:34 +0530

    Stage 2/7: Grader - Implementation completed

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -1,7 +1,34 @@
 package bullscows;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        //System.out.println("Hello World!");
+        var scanner = new Scanner(System.in);
+        var secretCode = new char[]{'9', '3', '0', '5'};
+        var answer = scanner.nextLine();
+        var bulls = 0;
+        var cows = 0;
+
+        for (int i = 0; i < secretCode.length; i++) {
+            if (secretCode[i] == answer.charAt(i)) {
+                bulls++;
+            } else if (answer.contains(String.valueOf(secretCode[i]))){
+                cows++;
+            }
+        }
+        if (bulls > 0) {
+            if (cows > 0) {
+                System.out.printf("Grade: %d bull(s) and %d cows(s). The secret code is 9305.", bulls, cows);
+            } else {
+                System.out.printf("Grade: %d bull(s). The secret code is 9305.", bulls);
+            }
+        } else if (cows > 0) {
+            System.out.printf("Grade: %d cows(s). The secret code is 9305.", cows);
+        } else {
+            System.out.printf("Grade: None. The secret code is 9305.");
+        }
     }
 }
